5e5a636f07015ad880926cf0dd03a6a8c83d9084
wuyf <1658956693@qq.com>
2017-09-22 14:52:19 +0800

    建库后首次提交，呀哈

---
+++ Assets/GetLocationInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetLocationInfo : MonoBehaviour
+{
+
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void StartLocation()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        text.text = jo.Call<string>("GetInfo");
+    }
+}
